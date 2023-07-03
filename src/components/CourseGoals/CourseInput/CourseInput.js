80c020b8d14fc397f46ee89928c024e4da6fbb8d
bubun123-git <bubundcool@gmail.com>
2023-07-04 01:32:03 +0530

    added some styles

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,11 +8,10 @@ const CourseInput = (props) => {
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
-    if(event.target.value.trim().length>0) {
-setIsValid(true)
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
     }
     setEnteredValue(event.target.value);
-    setIsValid(true);
   };
 
   const formSubmitHandler = (event) => {
@@ -28,9 +27,8 @@ setIsValid(true)
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
-        <label >Course Goal</label>
-        <input type='text' onChange={goalInputChangeHandler}
-        
+        <label>Course Goal</label>
+        <input type='text' onChange={goalInputChangeHandler} value={enteredValue} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
